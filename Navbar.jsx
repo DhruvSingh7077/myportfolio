ea5a1b389caba53291532eeec3f5c535dc967184
DhruvSingh7077 <dhruvsingh7077@gmail.com>
2025-07-03 23:18:47 +0530

    Add files via upload

---
+++ Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {FiMenu, Fix} from 'react-icons/fi';
+import {FaGithub, FaLinkedin} from 'react-icons/fa'
+const Navbar = () => {
+  return (
+    <nav className='bg-transparent'>
+        <div className='text-white py-5 flex justify-between items-center'>
+        <span className='text-[#8245ec]'>&lt;</span>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
