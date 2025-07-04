bd437b390c3d6bcb3a5976d3611a237feaa19ec3
DhruvSingh7077 <dhruvsingh7077@gmail.com>
2025-07-04 23:17:27 +0530

    Add files via upload

--- Navbar.jsx
+++ Navbar.jsx
@@ -26,7 +26,7 @@ useEffect(() => {
         {id:"experience", label:"Experience"},
         {id:"work", label:"Work"},
         {id:"education", label:"Education"},
-        {id:"about", label:"About"},
+        {id:"contact", label:"Contact"},
     ]
   return (
     <nav className={`fixed top-0 w-full z-50 transition duration-300 px-[7vw] lg:px-[20vw] ${isScrolled ? "bg-[#050414] bg-opacity-50 background-blur-md shadow-md" : "bg-transparent"}`}>
@@ -40,7 +40,7 @@ useEffect(() => {
             <span className='text-[#8245ec]'>&gt;</span>
             </div>
             {/* Desktop Menu */}
-            <ul className="md:flex space-x-8 text-gray-300">
+            <ul className=" hidden md:flex space-x-8 text-gray-300">
                 {menuItems.map((item)=>(
                     <li key={item.id} className={`cursor-pointer hover:text-[#8245ec] ${
                         activeSection === item.id? "text-[#8245ec]" : ""
@@ -51,7 +51,56 @@ useEffect(() => {
                     </li>
                 ))}
             </ul>
+            {/* social media icons*/}
+            <div className="hidden md:flex space-x-4">
+                <a
+                href=""
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-gray-300 hover:text-[#8245ec]"
+                >
+                    <FaGithub size={24} />
+                </a>
+                <a
+                href=""
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-gray-300 hover:text-[#8245ec]"
+                >
+                    <FaLinkedin size={24} />
+                </a>
+            </div>
+            {/* moblie Menu Icons */}
+            <div className="md:hidden">
+                {
+                    isOpen ? (
+                        <FiX className="text-3xl text-[#8245ec] cursor pointed"
+                        onClick={() => setIsOpen(false)} />
+                    )  : (
+                        <FiMenu className="text-3xl  text-[#8245ec] cursor-pointer"
+                        onClick={() => setIsOpen(true)} />
+                    )
+                    
+                }
+            </div>
         </div>
+        {/* Mobile menu Items */}
+        { isOpen && (
+            <div className="absolute top-16 left-1/2 transform- translate-x-1/2 w-4/5 bg-[#050414] bg-opacity-50 backdrop-filter backdrop-blur-lg z-50 rounded-lg shadow-lg">
+                <ul className="flex flex-col items-center space-y-4 py-4 text-gray-300">
+            {menuItems.map((item)=> (
+                <li key={item.id} className={`cursor-pointer hover:text-white
+                    ${activeSection === item.id ? " text-[#8245ec]": ""
+
+                    }`}>
+                        <button onClick={() => handleMenuItemClick}>
+                            {item.label}
+                        </button>
+                        </li>
+         )) }
+         </ul>
+         </div>
+        )}
     </nav>
   )
 }
